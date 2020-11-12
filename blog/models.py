53ad48d285cfc5a061b36e84b6cfb8b1c6c767e4
alirezaatabaki <alirezaatabaki@icloud.com>
2020-11-12 17:13:16 +0330

    add new manager to article model(024)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.utils import timezone
 from extensions.utils import jalali_converter
 
 
+# my managers
+class ArticleManager(models.Manager):
+    def published(self):
+        return self.filter(status='p')
+
+
 class Category(models.Model):
     title = models.CharField(max_length=200, verbose_name='عنوان دسته‌بندی ')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='اسلاگ')
@@ -49,3 +55,5 @@ class Article(models.Model):
 
     def category_published(self):
         return self.category.filter(status=True)
+
+    objects = ArticleManager( )
--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,19 @@ from .models import Article, Category
 
 def home(request):
     context = {
-        'articles': Article.objects.filter(status='p')
+        'articles': Article.objects.published()
     }
     return render(request, 'blog/home.html', context=context)
 
 
 def detail(request, slug):
     context = {
-        'article': get_object_or_404(Article, slug=slug, status='p')
+        'article': get_object_or_404(Article.objects.published(), slug=slug, status='p')
     }
     return render(request, 'blog/detail.html', context=context)
 
 
-def category(request,slug):
+def category(request, slug):
     context = {
         'category': get_object_or_404(Category, slug=slug, status=True)
     }
