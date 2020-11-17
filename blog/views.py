de245d1e0bebc8d519d9b95e9eaf96b6ebfd4e23
alirezaatabaki <alirezaatabaki@icloud.com>
2020-11-17 22:59:28 +0330

    writing home view with class base views (030)

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView
 
 from .models import Article, Category
 
 
-def home(request, page=1):
-    articles_list = Article.objects.published()
-    paginator = Paginator(articles_list, 3)
-    articles = paginator.get_page(page)
-    context = {
-        'articles': articles,
-    }
-    return render(request, 'blog/home.html', context=context)
+# def home(request, page=1):
+#     articles_list = Article.objects.published()
+#     paginator = Paginator(articles_list, 3)
+#     articles = paginator.get_page(page)
+#     context = {
+#         'articles': articles,
+#     }
+#     return render(request, 'blog/article_list.html', context=context)
+
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 3
 
 
 def detail(request, slug):
